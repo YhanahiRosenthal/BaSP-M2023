20a7d6b23c709acf5784c9d56e229ec36188cccf
YhanahiRosenthal <yhanahi16@gmail.com>
2023-04-14 17:56:42 -0300

    BaSP-W05-2023: 02-Strings

--- Week-05/exercises/01-variables-and-operators.js
+++ Week-05/exercises/01-variables-and-operators.js
@@ -1,17 +1,28 @@
-var a = 10
-var b = 34
-var c = a + b
+console.log("--EXERCISE 1: VARIABLES AND OPERATORS");
 
-console.log("Este es el resultado del ejercicio 'a' de variables y operadores: " + c)
+// a - Crear dos variables numéricas y utilizar el operador suma para guardar
+//     el valor de la suma de ambos números en una 3er variable.
 
-var wordOne = "Res"
-var wordTwo = "puesta"
-var result = wordOne + wordTwo
+var a = 10;
+var b = 34;
+var c = a + b;
 
-console.log("Este es el resultado del ejercicio 'b' de variables y operadores: " + result)
+console.log("-Excercise 1.a: " + c);
 
-var first = "Hola"
-var second = "Mundo"
-var wordSize = first.length + second.length
+// b - Crear dos variables de tipo String y
+//     concatenarlas guardando el resultado en una 3er variable.
 
-console.log("Este es el resultado del ejercicio 'c' de variables y operadores: " + wordSize)
+var wordOne = "Res";
+var wordTwo = "puesta";
+var result = wordOne + wordTwo;
+
+console.log("-Excercise 1.b: " + result);
+
+// c - Crear dos variables de tipo String y sumar el largo de cada variable
+//    (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+
+var first = "Hola";
+var second = "Mundo";
+var wordSize = first.length + second.length;
+
+console.log("-Excercise 1.c: " + wordSize);
---
+++ Week-05/exercises/02-strings.js
@@ -0,0 +1,58 @@
+console.log("--EXERCISE 2: STRINGS");
+
+// a - Crear una variable de tipo string con al menos 10 caracteres y
+//     convertir todo el texto en mayúscula (utilizar toUpperCase).
+
+var wordOfTenCharacters = "independencia";
+
+console.log("Exercise 2.a: " + wordOfTenCharacters.toUpperCase());
+
+// b - Crear una variable de tipo string con al menos 10 caracteres y generar un
+//     nuevo string con los primeros 5 caracteres guardando el resultado en una
+//     nueva variable (utilizar substring).
+
+var FirstNewString = wordOfTenCharacters.substring(0, 5)
+
+console.log("Exercise 2.b: " + FirstNewString);
+
+// c - Crear una variable de tipo string con al menos 10 caracteres y generar
+//     un nuevo string con los últimos 3 caracteres guardando el resultado en
+//     una nueva variable (utilizar substring).
+
+var SecondNewString = wordOfTenCharacters.substring(10)
+
+console.log("Exercise 2.c: " + SecondNewString);
+
+// d - Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string
+//     con la primera letra en mayúscula y las demás en minúscula.
+//     Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operador +).
+
+var OneLetter = wordOfTenCharacters.substring(0, 1);
+var RestOfWord = wordOfTenCharacters.substring(1, 13);
+
+console.log("Exercise 2.d: " + OneLetter.toUpperCase() + RestOfWord);
+
+// e - Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco.
+//     Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).
+
+var whiteSpace = "Hola Mundo";
+
+console.log(whiteSpace.indexOf(" "));
+
+// f - Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio).
+//     Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga
+//     la primera letra de ambas palabras en mayúscula y las demás letras en minúscula
+//     (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +).
+
+var twoWordsLongs = "contrarrevolucionario anticonstitucionalidad";
+
+var space = twoWordsLongs.indexOf(" ");
+var FirstCapitalLetter = twoWordsLongs.substring(0, 1).toUpperCase();
+var SecondCapitalLetter = twoWordsLongs.substring(22, 23).toUpperCase();
+
+var RestOfFirstWord = twoWordsLongs.substring(1, 21);
+var RestOfSecondWord  = twoWordsLongs.substring(23, 44);
+
+
+
+console.log("Exercise 2.d: " + FirstCapitalLetter + RestOfFirstWord + " " + SecondCapitalLetter + RestOfSecondWord);
