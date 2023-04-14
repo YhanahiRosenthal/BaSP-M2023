c49e106854f8561344e61e4bb284e59f7f578779
YhanahiRosenthal <yhanahi16@gmail.com>
2023-04-14 11:02:32 -0300

    BaSP-W05-2023: 01-variables-and-operators

---
+++ Week-05/exercises/01-variables-and-operators.js
@@ -0,0 +1,17 @@
+var a = 10
+var b = 34
+var c = a + b
+
+console.log("Este es el resultado del ejercicio 'a' de variables y operadores: " + c)
+
+var wordOne = "Res"
+var wordTwo = "puesta"
+var result = wordOne + wordTwo
+
+console.log("Este es el resultado del ejercicio 'b' de variables y operadores: " + result)
+
+var first = "Hola"
+var second = "Mundo"
+var wordSize = first.length + second.length
+
+console.log("Este es el resultado del ejercicio 'c' de variables y operadores: " + wordSize)
