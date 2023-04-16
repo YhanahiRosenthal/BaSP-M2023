c6eb27a8b95e3e0d901991f3ab4acc979aef06fa
YhanahiRosenthal <yhanahi16@gmail.com>
2023-04-16 15:46:03 -0300

    BaSP-W05-2023: corrections made on css and footer of html

--- Week-05/exercises/05-for.js
+++ Week-05/exercises/05-for.js
@@ -28,6 +28,7 @@ for(var i = 0; i < words.length; i++){
     };
 alert(sentence);
 
+
 // d - Crear un array vacío y con un bucle for de 10 repeticiones. Llenar el array con el número de la repetición,
 //     es decir que al final de la ejecución del bucle for debería haber 10 elementos dentro del array, desde el
 //     número 0 hasta al número 9. Mostrar por la consola del navegador el array final (utilizar console.log).
